eed8aaa894dbdf475f68495a56d629de92560aa7
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2023-05-05 02:47:32 +0200

    plotting already but rotation is whack

---
+++ main.js
@@ -0,0 +1,341 @@
+
+
+
+
+
+
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+// 3D ELEMENTS
+//////////////////////////////
+//////////////////////////////
+
+
+//functions for orthographic projection
+
+function rotateX(NODE, theta) {
+    var sinTheta = Math.sin(-theta);
+    var cosTheta = Math.cos(-theta);
+    var y = NODE.y;
+    var z = NODE.z;
+    NODE.y = y * cosTheta - z * sinTheta;
+    NODE.z = z * cosTheta + y * sinTheta;
+}
+
+function rotateY(NODE, theta) {
+    var sinTheta = Math.sin(-theta);
+    var cosTheta = Math.cos(-theta);
+    var x = NODE.x;
+    var z = NODE.z;
+    NODE.x = x * cosTheta - z * sinTheta;
+    NODE.z = z * cosTheta + x * sinTheta;
+}
+
+function rotateZ(NODE, theta) {
+    var sinTheta = Math.sin(theta);
+    var cosTheta = Math.cos(theta);
+    var x = NODE.x;
+    var y = NODE.y;
+    NODE.x = x * cosTheta - y * sinTheta;
+    NODE.y = y * cosTheta + x * sinTheta;
+}
+
+
+// make a class for the nodes
+class Node {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+
+
+// make a point element
+class Point {
+    constructor(props, x, y, z) {
+        //set default properties
+        this.props = {
+            color: 'black',
+            size: 1,
+            fill: true,
+            stroke: true,
+            strokeColor: 'black',
+            strokeWidth: 1,
+            opacity: 1
+        };
+        //set properties from props
+        for (var p in props) {
+            this.props[p] = props[p];
+        }
+        this.nodes = [];
+        this.nodes.push(new Node(x, y, z));
+        ELEMENTS.push(this);
+    }
+    draw(scale) {
+        var node = this.nodes[0];
+        var x = node.x * scale;
+        var y = node.y * scale;
+        var z = node.z;
+        var size = this.props.size;
+        var color = this.props.color;
+        var fill = this.props.fill;
+        var stroke = this.props.stroke;
+        var strokeColor = this.props.strokeColor;
+        var strokeWidth = this.props.strokeWidth;
+        var opacity = this.props.opacity;
+        ctx.beginPath();
+        ctx.arc(x, y, size, 0, 2 * Math.PI);
+        ctx.closePath();
+        if (fill) {
+            ctx.fillStyle = color;
+            ctx.globalAlpha = opacity;
+            ctx.fill();
+        }
+        if (stroke) {
+            ctx.strokeStyle = strokeColor;
+            ctx.lineWidth = strokeWidth;
+            ctx.globalAlpha = opacity;
+            ctx.stroke();
+        }
+    }
+}
+
+// make a line element
+class Line {
+    constructor(props, x1, y1, z1, x2, y2, z2) {
+        //set default properties
+        this.props = {
+            color: 'black',
+            size: 1,
+            fill: true,
+            stroke: true,
+            strokeColor: 'black',
+            strokeWidth: 1,
+            opacity: 1
+        };
+        //set properties from props
+        for (var p in props) {
+            this.props[p] = props[p];
+        }
+        this.nodes = [];
+        this.nodes.push(new Node(x1, y1, z1));
+        this.nodes.push(new Node(x2, y2, z2));
+        ELEMENTS.push(this);
+    }
+    draw(scale) {
+        var node1 = this.nodes[0];
+        var node2 = this.nodes[1];
+        var x1 = node1.x * scale;
+        var y1 = node1.y * scale;
+        var z1 = node1.z;
+        var x2 = node2.x * scale;
+        var y2 = node2.y * scale;
+        var z2 = node2.z;
+        var color = this.props.color;
+        var fill = this.props.fill;
+        var stroke = this.props.stroke;
+        var strokeColor = this.props.strokeColor;
+        var strokeWidth = this.props.strokeWidth;
+        var opacity = this.props.opacity;
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.closePath();
+        if (fill) {
+            ctx.fillStyle = color;
+            ctx.globalAlpha = opacity;
+            ctx.fill();
+        }
+        if (stroke) {
+            ctx.strokeStyle = strokeColor;
+            ctx.lineWidth = strokeWidth;
+            ctx.globalAlpha = opacity;
+            ctx.stroke();
+        }
+    }
+}
+
+
+
+
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+var canvas = document.getElementById('myCanvas');
+
+var ctx = canvas.getContext('2d');
+ctx.translate(canvas.width / 2, canvas.height / 2);
+
+//set initial rotation
+var rotX = 0 * Math.PI/180; //rotation around x-axis
+var rotY = 0 * Math.PI/180; //rotation around y-axis
+var rotZ = 0; //rotation around z-axis
+
+//set initial scale
+var scale = 50;
+
+
+//function sizeCanvas
+function sizeCanvas() {
+    var cs = getComputedStyle(document.getElementById('container'));
+    canvas.width = parseInt(cs.getPropertyValue('width'), 10);
+    canvas.height = parseInt(cs.getPropertyValue('height'), 10);
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+}
+
+sizeCanvas();
+
+//set up event listeners
+var mouseX = 0,
+    mouseY = 0,
+    pmouseX, pmouseY; // mouseX is current mouse x-coordinate, pmouseX is previous mouse x-coordinate when it was 1 pixel different.  
+
+var mouseDown = false;
+
+canvas.addEventListener('mousedown', handleMousedown, false);
+canvas.addEventListener('touchstart', handleMousedown, false);
+canvas.addEventListener('mouseup', function (e) {
+    if (mouseDown) {
+        mouseDown = false;
+    }
+}, false);
+canvas.addEventListener('mousemove', handleMousemove, false);
+canvas.addEventListener('touchmove', handleMousemove, false);
+//canvas.addEventListener('wheel', handleMousewheel, false);
+
+
+function handleMousedown(e) {
+    if (!mouseDown) {
+        mouseDown = true;
+    }
+    var x = parseInt(e.clientX) || parseInt(e.touches[0].clientX);
+    var y = parseInt(e.clientY) || parseInt(e.touches[0].clientY);
+
+    mouseX = x;
+    mouseY = y;
+    e.preventDefault();
+}
+
+function handleMousemove(e) {
+    var sens = 10;
+    pmouseX = mouseX;
+    pmouseY = mouseY;
+    var x = parseInt(e.pageX) || parseInt(e.touches[0].clientX);
+    var y = parseInt(e.pageY) || parseInt(e.touches[0].clientY);
+    if (Math.abs(x - pmouseX) >= 1) {
+        mouseX = x;
+    } else {
+        mouseX = pmouseX;
+    }
+    if (Math.abs(y - pmouseY) >= 1) {
+        mouseY = y;
+    } else {
+        mouseY = pmouseY;
+    }
+
+    //change rotation
+    if (mouseDown===true) {
+        rotX += (mouseY - pmouseY) / 10000;
+        rotY += (mouseX - pmouseX) / 10000;
+        render();
+    }
+    e.preventDefault();
+}
+
+//set up event listeners for arrow keys
+document.addEventListener('keydown', function (e) {
+    var sens = 10;
+    if (e.keyCode == 37) {
+        //left arrow
+        rotY -= 0.1/sens;
+        render();
+    } else if (e.keyCode == 38) {
+        //up arrow
+        rotX += 0.1/sens;
+        render();
+    } else if (e.keyCode == 39) {
+        //right arrow
+        rotY += 0.1/sens;
+        render();
+    } else if (e.keyCode == 40) {
+        //down arrow
+        rotX -= 0.1/sens;
+        render();
+    }
+
+}, false);
+
+
+
+
+
+
+
+
+function initialRender(){
+    sizeCanvas();
+    render();
+}
+
+window.addEventListener('resize', initialRender, false);
+
+
+
+
+//render function
+var ELEMENTS = [];
+
+function render() {
+    //start performance timer
+    var t0 = performance.now();
+    //clear canvas
+    ctx.clearRect(-canvas.width, -canvas.height, 2 * canvas.width, 2 * canvas.height);
+
+  
+    for (var i = 0; i < ELEMENTS.length; i++) {
+        //rotate the nodes
+        for (var j = 0; j < ELEMENTS[i].nodes.length; j++) {
+            //console.log(rotX, rotY, rotZ);
+            rotateX(ELEMENTS[i].nodes[j], rotX);
+            rotateY(ELEMENTS[i].nodes[j], rotY);
+            rotateZ(ELEMENTS[i].nodes[j], rotZ);
+        }
+        //draw the element
+        ELEMENTS[i].draw(scale);
+    }
+    //end performance timer
+    var t1 = performance.now();
+    console.log("Render took " + (t1 - t0) + " milliseconds.")
+}
+
+
+
+//render
+render();
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,1 +1,266 @@
-const canvas = document.getElementById('canvas');
+"use strict";
+
+// "pixel" dimensions 
+var pixelWidth = 2;
+var scale = 50; // a scale factor
+// nodes settings
+var xMin = -5;
+var xMax = 5;
+var yMin = -5;
+var yMax = 5;
+
+// rotation angles [rad]
+var rotx = 300 * Math.PI/180;
+var roty = 25 * Math.PI/180;
+var rotz = 0 * Math.PI/180;
+
+var nodes =[];
+var nodesAxes =[];
+
+var cont = document.getElementById('container');
+// Obtain a reference to the canvas element.
+var canvas  = document.getElementById("myCanvas");
+// Obtain a 2D context from the canvas element.
+var cxt = canvas.getContext("2d");
+
+
+cxt.lineCap = "round";
+cxt.lineJoin = "round";
+cxt.font = 'normal 16px Inter';
+
+sizeCanvas();	
+	
+// *** Mouse rotate. ***	
+var mouseX = 0, mouseY = 0, pmouseX, pmouseY; // mouseX is current mouse x-coordinate, pmouseX is previous mouse x-coordinate when it was 1 pixel different.  
+// Figure must rotate iff mousemove AND mousedown (for mouse devices):
+var mouseDown = false;
+cont.addEventListener("mousedown", handleMousedown, false);	
+cont.addEventListener("touchstart", handleMousedown, false);	
+document.addEventListener("mouseup", function (e) { if(mouseDown) mouseDown = false; canvas.style.cursor = "grab"; }, false);	
+cont.addEventListener("mousemove", handleMove, false);
+cont.addEventListener("touchmove", handleMove, false);
+
+//Zoom with mouse wheel
+canvas.addEventListener("wheel", function(e) {
+    if (e.deltaY < 0) {
+        scale *= 1.1;
+    }
+    else {
+        scale /= 1.1;
+    }
+    drawFunction();
+    e.preventDefault();
+});
+
+
+
+function handleMousedown(e) {
+	if(!mouseDown) mouseDown = true;
+	canvas.style.cursor = "grabbing";
+	var x = parseInt(e.pageX) || parseInt(e.changedTouches[0].pageX);
+	var y = parseInt(e.pageY) || parseInt(e.changedTouches[0].pageY);
+	mouseX = x;
+	mouseY = y;
+	e.preventDefault(); 
+};
+  
+function handleMove(e) {
+	var sensetivity = 100; // how sensitive the mouse should be
+
+	// getting mouseX, mouseY, pmouseX and pmouseY.	
+	pmouseX = mouseX;
+	pmouseY = mouseY;
+	var x = parseInt(e.pageX) || parseInt(e.changedTouches[0].pageX);
+	var y = parseInt(e.pageY) || parseInt(e.changedTouches[0].pageY);	
+	if (Math.abs(x - pmouseX) >= 1) {mouseX = x;} else { mouseX = pmouseX }
+	if (Math.abs(y - pmouseY) >= 1) {mouseY = y;} else { mouseY = pmouseY }	
+
+	// change rotation
+	//if(e.which==1 || e.buttons==1) {
+	if(mouseDown === true) {
+		rotx = rotx + (mouseY - pmouseY)/sensetivity;
+		roty = roty + (mouseX - pmouseX)/sensetivity;
+		drawFunction();
+	}
+	e.preventDefault();
+};
+// *** END Mouse rotate. ***
+
+
+
+drawFunction();
+
+window.addEventListener("resize", initial);
+
+function initial() {
+	sizeCanvas();
+	drawFunction();
+};
+
+function sizeCanvas() {	
+	/// Make size of canvas equal to size container and move canvas to center
+	var cs = getComputedStyle(cont);
+	/// these will return dimensions in *pixel* regardless of what
+	/// you originally specified for container:
+	canvas.width = parseInt(cs.getPropertyValue('width'), 10);
+	canvas.height = parseInt(cs.getPropertyValue('height'), 10);
+	cxt.translate(canvas.width/2, canvas.height/2);	
+};	
+
+function drawFunction() {
+
+    //start performance timer
+    var t0 = performance.now();
+
+	cxt.clearRect(-canvas.width, -canvas.height, 2*canvas.width, 2*canvas.height);
+
+    function fx(t){
+        return 2*Math.sin(t)
+    }
+    function fy(t){
+        return 2*Math.cos(t)
+    }
+    function fz(t){
+        return t/5
+    }
+
+    var i=0;
+    var dt=0.01;
+    var z_d = 1;   // move the center of the graph along z-axis
+    for (var t = -Math.PI; t <= 6*Math.PI; t += dt) {
+        nodes[i] = {x:fx(t), y:fy(t), z:fz(t)-z_d};
+        ++i;
+    }
+    
+	nodesAxes[0] = {x:0, y:0, z:-z_d};
+	nodesAxes[1] = {x:xMax, y:0, z:-z_d};
+	nodesAxes[2] = {x:0, y:yMax, z:-z_d};
+	nodesAxes[3] = {x:0, y:0, z:xMax-z_d};
+	
+	// rotate nodes
+	rotateX3D(rotx);	
+	rotateY3D(roty);
+	rotateZ3D(rotz);
+    //zoom3D(scale);
+	
+
+    // draw axes
+	cxt.strokeStyle = "black";
+    cxt.lineWidth = 1;
+    cxt.font = 'normal 14px Inter';
+	cxt.beginPath();
+	cxt.moveTo(nodesAxes[0].x*scale,nodesAxes[0].y*scale);
+	cxt.lineTo(nodesAxes[1].x*scale,nodesAxes[1].y*scale);
+	cxt.stroke();      
+    cxt.fillText('x',nodesAxes[1].x*scale,nodesAxes[1].y*scale);	
+	cxt.beginPath();
+	cxt.moveTo(nodesAxes[0].x*scale,nodesAxes[0].y*scale);
+	cxt.lineTo(nodesAxes[2].x*scale,nodesAxes[2].y*scale);
+	cxt.stroke();
+    cxt.fillText('y',nodesAxes[2].x*scale,nodesAxes[2].y*scale);		
+	cxt.beginPath();
+	cxt.moveTo(nodesAxes[0].x*scale,nodesAxes[0].y*scale);
+	cxt.lineTo(nodesAxes[3].x*scale,nodesAxes[3].y*scale);
+	cxt.stroke();
+    cxt.fillText('z',nodesAxes[3].x*scale,nodesAxes[3].y*scale);	
+
+
+
+    
+	// draw figure
+	
+	for (var i=0; i < nodes.length; i++) {
+        cxt.beginPath();
+		cxt.strokeStyle = `hsl(${i/nodes.length*360}, 100%, 50%)`
+        cxt.lineWidth = 4;
+		var px = nodes[i].x;
+		var py = nodes[i].y;
+
+		//cxt.fillRect(px*scale,py*scale,pixelWidth,pixelWidth)
+
+        cxt.lineTo(px*scale,py*scale);
+        if(i<nodes.length-1){
+            var px2 = nodes[i+1].x;
+        var py2 = nodes[i+1].y;
+        cxt.lineTo(px2*scale,py2*scale);
+        }
+        cxt.stroke();
+	}
+
+    //end performance timer
+    var t1 = performance.now();
+    console.log("Call to draw took " + (t1 - t0) + " milliseconds.")
+
+};
+
+// Rotate shape around the z-axis, i.e. the non-rotated axis, perpendicular to the screen.
+function rotateZ3D(theta) {
+    var sinTheta = Math.sin(theta);
+    var cosTheta = Math.cos(theta);
+    
+    for (var n=0; n<nodes.length; n++) {
+        var node = nodes[n];
+        var x = node.x;
+        var y = node.y;
+        node.x = x * cosTheta - y * sinTheta;
+        node.y = y * cosTheta + x * sinTheta;
+    }
+    for (n=0; n<nodesAxes.length; n++) {
+        node = nodesAxes[n];
+        x = node.x;
+        y = node.y;
+        node.x = x * cosTheta - y * sinTheta;
+        node.y = y * cosTheta + x * sinTheta;
+    }	
+};
+
+// Rotate shape around the y-axis, i.e. the non-rotated axis, vertical to the screen.
+function rotateY3D(theta) {
+    var sinTheta = Math.sin(-theta);
+    var cosTheta = Math.cos(-theta);
+    
+    for (var n=0; n<nodes.length; n++) {
+        var node = nodes[n];
+        var x = node.x;
+        var z = node.z;
+        node.x = x * cosTheta - z * sinTheta;
+        node.z = z * cosTheta + x * sinTheta;
+    }
+    for (n=0; n<nodesAxes.length; n++) {
+        node = nodesAxes[n];
+        x = node.x;
+        z = node.z;
+        node.x = x * cosTheta - z * sinTheta;
+        node.z = z * cosTheta + x * sinTheta;
+    }	
+};
+
+// Rotate shape around the x-axis, i.e. the non-rotated axis, horizontal to the screen.
+function rotateX3D(theta) {
+    var sinTheta = Math.sin(-theta);
+    var cosTheta = Math.cos(-theta);
+    
+    for (var n=0; n<nodes.length; n++) {
+        var node = nodes[n];
+        var y = node.y;
+        var z = node.z;
+        node.y = y * cosTheta - z * sinTheta;
+        node.z = z * cosTheta + y * sinTheta;
+    }
+    for (n=0; n<nodesAxes.length; n++) {
+        node = nodesAxes[n];
+        y = node.y;
+        z = node.z;
+        node.y = y * cosTheta - z * sinTheta;
+        node.z = z * cosTheta + y * sinTheta;
+    }	
+};
+
+
+
+
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
+
